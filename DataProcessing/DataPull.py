a26a82c8ee088656daa8a52db66ead179fe9f3d5
Jack McSweeney <jmcsweeney34@gmail.com>
2019-10-12 12:29:18 -0600

    Modulizing AlongShelf project

--- DataProcessing/DataPull.py
+++ DataProcessing/DataPull.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+"""
+Specify what data you want to upload from the Inner Shelf Project 
+
+written by Jack McSweeney (help from Zech Thurman) 
+October 12, 2019 
+"""
+
+def DataPull():
+    
+    return 
