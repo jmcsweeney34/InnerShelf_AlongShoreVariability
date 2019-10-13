98a7fab79085c7132de390407bfce61bdc8b09ec
Jack McSweeney <jmcsweeney34@gmail.com>
2019-10-13 09:16:30 -0600

    Loadmat files of interest

--- DataProcessing/DataPull.py
+++ DataProcessing/DataPull.py
@@ -6,6 +6,25 @@ written by Jack McSweeney (help from Zech Thurman)
 October 12, 2019 
 """
 
-def DataPull():
+#def DataPull():
+from scipy.io import loadmat
+import numpy as np 
+
+dir= '/Volumes/InnerShelf1/JackAnalysis/McSweeney_data/InnerShelf_AlongShoreVariability/'
+
+fnames= np.array(['MS100','OC50','OC40N'])
+
+#loadedmats={}
+fext='.mat'
+
+for moor in fnames:
+    filepath=(dir+'Moorings/'+moor+fext)
+    print(filepath)
+#    loadedmats["{0}".format(moor)]=loadmat(filepath)
+    globals()[moor] = loadmat(filepath)
     
-    return 
+
+    
+#    print()
+    
+#    return 
